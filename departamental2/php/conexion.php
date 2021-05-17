62661f3d3fab05a9279452b3c496998c7ba1446c
AldoCA27 <72528576+AldoCA27@users.noreply.github.com>
2021-05-17 18:02:47 -0500

    Delete departamental2 directory

--- departamental2/php/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $servidor = "localhost";
-    $usuario = "root";
-    $clave = "";
-    $base = "electivas";
-
-    $conexion =mysqli_connect($servidor,$usuario,$clave,$base);
-    mysqli_set_charset($conexion,"utf8");
-?>
